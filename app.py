f015edc5ea7137835969e7b07373a21733b75d50
Jedrzej Kopiszka <jedrzej.kopiszka@tractive.com>
2023-08-19 14:48:35 +0200

    Check for existing user and redirect to registration from login page

--- app.py
+++ app.py
@@ -53,8 +53,12 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
         
-        hashed_password = generate_password_hash(password, method='sha256')
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            return jsonify({'status': 'failure', 'message': 'User already exists'})
         
+        hashed_password = generate_password_hash(password, method='sha256')
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
